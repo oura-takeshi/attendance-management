b7df587bf89b508f94368dde9615b3b7855c3f1d
oura takeshi <laughseed@gmail.com>
2025-10-09 21:27:44 +0900

    認証機能（一般ユーザー）のテストケースの修正

--- src/tests/Feature/User/Auth/RegisterTest.php
+++ src/tests/Feature/User/Auth/RegisterTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature\User\Auth;
 use App\Models\User;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
