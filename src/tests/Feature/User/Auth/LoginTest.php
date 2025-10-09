1f0db4e3debb0b1d62b22c5487e0dc0395de6248
oura takeshi <laughseed@gmail.com>
2025-10-09 22:20:14 +0900

    ログイン認証機能（一般ユーザー）のテストケースの実装

--- src/tests/Feature/User/Auth/LoginTest.php
+++ src/tests/Feature/User/Auth/LoginTest.php
@@ -2,21 +2,74 @@
 
 namespace Tests\Feature\User\Auth;
 
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(DatabaseSeeder::class);
+    }
+
+    public function test_login_user()
+    {
+        $user = User::find(1);
+
+        $response = $this->post('/login', [
+            'email' => "hoge@example.com",
+            'password' => "hoge1234",
+        ]);
+
+        $response->assertRedirect('/attendance');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_login_user_validate_email()
+    {
+        $response = $this->post('/login', [
+            'email' => "",
+            'password' => "hoge1234",
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+
+        $errors = session('errors');
+        $this->assertEquals('メールアドレスを入力してください', $errors->first('email'));
+    }
+
+    public function test_login_user_validate_password()
+    {
+        $response = $this->post('/login', [
+            'email' => "hoge@example.com",
+            'password' => "",
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('password');
+
+        $errors = session('errors');
+        $this->assertEquals('パスワードを入力してください', $errors->first('password'));
+    }
+
+    public function test_login_user_validate_user()
+    {
+        $response = $this->post('/login', [
+            'email' => "hoge@example.com",
+            'password' => "hoge5678",
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('message');
+
+        $message = session('message');
+        $this->assertEquals('ログイン情報が登録されていません', $message);
     }
 }
